1d1f7b4fb02317e614dd1f27120651c069fd4d8e
Lu Gan <lu.gan@gesis.org>
2023-08-17 16:54:57 +0200

    update codes

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import json
 
 from bs4 import BeautifulSoup
 from pprint import pprint
+import pandas as pd
 from tqdm import tqdm
-
+from utils import hyperlink_extraction
 
 # -------------------------
 # use beautifulsoup
@@ -30,9 +31,9 @@ def bs4_find(text, target, patterns=None):
 
 README_JSON_XML_FILE = './data/readme2_v3.json'
 
-with open(README_JSON_XML_FILE, 'r') as fr:
-    readmes_xml = json.load(fr)
-readmes_xml_noempty = {int(k):v for k,v in readmes_xml.items() if not v == ''}
+# with open(README_JSON_XML_FILE, 'r') as fr:
+#    readmes_xml = json.load(fr)
+# readmes_xml_noempty = {int(k):v for k,v in readmes_xml.items() if not v == ''}
 ##  pprint(list(readmes_xml_noempty.items())[:1])
 
 # -------------------
@@ -51,7 +52,7 @@ def extract_block():
 
     # with open('textblocks.json', 'w') as fw:
     #    json.dump(block_dict, fw, indent=4)
-extract_block()
+# extract_block()
 
 # --------------------
 # link extraction
@@ -75,3 +76,29 @@ def extract_link():
         json.dump(link_dict, fw, indent=4)
 
 # extract_link()
+
+# ------------- 
+# read & process links.json
+# -------------
+LINKS_JSON_FILE = './res/links.json'
+with open(LINKS_JSON_FILE, 'r') as fr:
+    links_dict = json.load(fr)
+links_strs = [v for k,v in links_dict.items() if len(v) > 0]
+print(len(links_strs))
+links, texts, prev_texts, next_texts = [], [], [], []
+for link_str in links_strs:
+    for _link_str in link_str:
+        _link, _text, _prev_text, _next_text = hyperlink_extraction(_link_str)
+        if _link[-3:] == 'pdf' or _link[-3:]=='pth' or _link[-3:]=='log':
+            continue
+        links.append(_link)
+        texts.append(_text)
+        prev_texts.append(_prev_text)
+        next_texts.append(_next_text)
+
+links_extract_df = pd.DataFrame(data={'link':links, 'text':texts,
+    'prev_text': prev_texts, 'next_text': next_texts})
+print(links_extract_df['text'].head(20))
+print(len(links_extract_df))
+links_extract_df.to_csv('./res/links_extraction_no_pdf_link.csv', index=False)
+print(len(links_extract_df['link'].unique()))
---
+++ utils.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+
+
+# extract link, text, previous text and next text from hyperlink strings
+def hyperlink_extraction(hl_str):
+    soup = BeautifulSoup(hl_str, features='lxml-xml')
+    matches = soup.find_all('link')
+    if len(list(matches)) < 1:
+        return None, None, None, None
+    elif len(list(matches)) == 1:
+        for _match in matches:
+            link = _match.attrs['destination']
+            text = _match.text
+            prev_text = _match.previousSibling
+            next_text = _match.nextSibling
+        return link, text, prev_text, next_text
+    else:
+        for _match in matches:
+            pass
+        pass
+
+# link_str = "<link destination=\"http://pandas.pydata.org/\">pandas</link>"
+# print(hyperlink_extraction(link_str))
